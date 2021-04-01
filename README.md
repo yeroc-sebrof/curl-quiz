cd954f0f424d4f839a64d8158fe7d4dca47f2f99
1320071 <1700231@uad.ac.uk>
2021-04-01 16:04:16 +0100

    Minor changes: - css colour changes to make webpage easier to read + make hyperlinks more obvious - Added hint to challenge 6 - Added linebreak to CLI errors - Made use wget/curl error clearer - Removed confusing large error - Hidden answers in README behind collapsable span - Edited hint hint to make it clearer - Minor grammar alterations

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
 This lab should be easy enough, apply some CLI knowledge and the man page for curl to get the encoding/encryption challenges. Once you have the challenge you need to decode it to get the flag. Your target is the web server at 192.168.22.100
 
-
+---
 
 ### Challenge 1 - Different port + GET Param and Atbash
 
 To get the encoding challenge for this one send a GET request to 192.168.22.100/challenge1 with a two parameters, one called whoAreYou with the value AHacker and the other called areYouSure with the value yes
 
-##### Answer
+<details>
+ <summary markdown="span">Answer</summary>
 
 `curl http://192.168.22.100/challenge1?whoAreYou=AHacker&areYouSure=yes`
 
@@ -16,50 +17,64 @@ To get the encoding challenge for this one send a GET request to 192.168.22.100/
 - Atbash is the reversing of an alphabet to make z>a == a>z
     - http://rumkin.com/tools/cipher/atbash.php
 - Resolves to: super-cool-classic-cipher
+</details>
 
+---
 
 ### Challenge 2 - HTTP/1.0 and Caesar Cipher
 
 To get this encoding challenge make a HTTP/1.0 request to http://192.168.22.100/challenge2. Curl will default to doing a HTTP/1.1 request
 
-##### Answer
+<details>
+ <summary markdown="span">Answer</summary>
 
 `curl --http1.0 http://172.17.0.2/challenge2`
 
 - Challenge String: `MXE-MEKBT-XQLU-ADEMD-JXQJ-ZKBYKI-SQUIQH'I-SYFXUH-MEKBT-RU-IE-FEFKBQH-QBB-JXUIU-OUQHI-BQJUH.QDOMQO-LQKBJYDW-TYIJEHJ-THKCCEDT-YI-JXU-VBQW.AUUF-JXU-TQIXUI`
 - A Rotation of 10 characters using the Caesar/Rot13 Cipher
 - Resolves to: `Who would have known that Julius Caesar's cipher would be so popular all these years later. Anyway Vaulting Distort Drummond is the flag. Keep the dashes`
+</details>
+
+---
 
 ### Challenge 3 - PATCH request and MD5 secret
 
 HTTP 1.1 has a fair few methods that are documented but anyone could make any method they wanted. In this case make a HTTP PATCH request to http://192.168.22.100/challenge3
 
-##### Answer
+<details>
+ <summary markdown="span">Answer</summary>
 
 `curl -X PATCH http://172.17.0.2/challenge3`
 
 - Challenge String: 5c577753706cdcef8621a9c0c1922158
 - https://www.md5online.org/md5-decrypt.html is the first result on Google when searching "md5 decrypt"
 - Decodes to: md5 is broken
+</details>
+
+---
 
 ### Challenge 4 - POST Request with JSON content and Base64 + Base85
 
 POST some JSON content for this one (in a similar fashion to above where encoding was set to whoAreYou) now in your POST Requests JSON body, set "WeGonnaRockDownTo" to "ElectricBoogaloo"
 
-##### Answer
+<details>
+ <summary markdown="span">Answer</summary>
 
 `curl -X POST -H "Content-Type: application/json" -d '{"WeGonnaRockDownTo":"ElectricBoogaloo"}' http://172.17.0.2/challenge4`
 
 - Challenge String: `;f#3*<,"[<@sLCU@PTrb`
 - Base85 Decode to: `SlNPTiBpcyBjb29s`
 - Base64 Decode to: `JSON is cool`
+</details>
 
+---
 
 ### Challenge 5 - Request a different TCP Port and Vigenere Cipher
 
 An easy challenge now that we're near the end. Send a GET request to port 8080 for this one
 
-##### Answer
+<details>
+ <summary markdown="span">Answer</summary>
 
 `curl "http://172.17.0.2:8080/challenge5"`
 
@@ -67,16 +82,20 @@ An easy challenge now that we're near the end. Send a GET request to port 8080 f
 - https://www.boxentriq.com/code-breaking/vigenere-cipher has a auto-solver
 - The secret key is `ababdcdc`
 - Decodes to: `vigenere is a pretty cool tool`
+</details>
+
+---
 
 ### Challenge 6 - PUT and XOR
 
 Start by running `echo "superMegaFinalFlagFile" > file.txt` then upload that file using the HTTP PUT method to challenge6
 
-##### Answer
+<details>
+ <summary markdown="span">Answer</summary>
 
 `curl -T ./file.txt -X PUT http://172.18.0.2/challenge6` or `curl --upload-file ./file.txt -X PUT http://172.18.0.2/challenge6`
 
 - Challenge String: `07 0d 0a 06 72 1d 1c 17 63 10 1c 06 1c 01 0a 1b 15 65 00 11 16 13 14 65 1a 16 63 02 1b 09 17 65 0a 75 11 04 1d 11 63 17 17 09 1a 13 06 75 1a 0a 04 65 14 10 1e 09 73 0c 17 75 05 0a 01 0e 10 75 13 0b 17 65 0b 1a 05 65 00 00 00 00 00 00 73 0c 17 75 1b 16 73 11 0c 1a 7c 65 1d 0a 17 1c 11 00 73 0d 0c 02 72 0c 73 10 10 10 72 11 1b 00 63 1e 17 1c 73 0c 0d 75 06 0d 16 65 17 10 0a 11 73 04 0d 11 72 11 1b 00 0d 75 06 0d 16 65 17 10 0a 11 73 11 16 07 1c 16 73 0c 0d 01 1d 65 63 62 10 75 13 0b 0a 12 02 0c 72 11 1b 00 63 13 1e 04 14 65 0a 06 72 1d 1c 17 6e 1c 01 68 10 0a 0c 19 7c 65 12 0b 07 75 06 0d 12 11 64 06 72 11 1b 00 63 13 1b 0b 12 09 63 13 1e 04 14 65 0c 13 72 11 1b 0c 10 75 11 0d 12 09 0f 10 1c 02 16 65 0a 75 1a 0a 03 00 63 0c 1d 10 74 13 06 75 17 0b 19 0a 1a 10 16 65 12 0b 07 75 1b 65 12 09 10 1a 72 0d 1c 15 06 75 06 0d 1a 16 63 1c 01 65 16 0b 0c 00 15 0d 73 11 06 0d 06 65 07 0a 63 18 13 0e 16 65 1b 1a 00 65 11 17 16 01 17 65 15 0a 11 16 17 65 12 0b 63 1a 02 11 1a 0a 0d`
 - `https://gchq.github.io/CyberChef/#recipe=From_Hex('Auto')XOR(%7B'option':'UTF8','string':'SECURE'%7D,'Standard',false)&input=MDcgMGQgMGEgMDYgNzIgMWQgMWMgMTcgNjMgMTAgMWMgMDYgMWMgMDEgMGEgMWIgMTUgNjUgMDAgMTEgMTYgMTMgMTQgNjUgMWEgMTYgNjMgMDIgMWIgMDkgMTcgNjUgMGEgNzUgMTEgMDQgMWQgMTEgNjMgMTcgMTcgMDkgMWEgMTMgMDYgNzUgMWEgMGEgMDQgNjUgMTQgMTAgMWUgMDkgNzMgMGMgMTcgNzUgMDUgMGEgMDEgMGUgMTAgNzUgMTMgMGIgMTcgNjUgMGIgMWEgMDUgNjUgMDAgMDAgMDAgMDAgMDAgMDAgNzMgMGMgMTcgNzUgMWIgMTYgNzMgMTEgMGMgMWEgN2MgNjUgMWQgMGEgMTcgMWMgMTEgMDAgNzMgMGQgMGMgMDIgNzIgMGMgNzMgMTAgMTAgMTAgNzIgMTEgMWIgMDAgNjMgMWUgMTcgMWMgNzMgMGMgMGQgNzUgMDYgMGQgMTYgNjUgMTcgMTAgMGEgMTEgNzMgMDQgMGQgMTEgNzIgMTEgMWIgMDAgMGQgNzUgMDYgMGQgMTYgNjUgMTcgMTAgMGEgMTEgNzMgMTEgMTYgMDcgMWMgMTYgNzMgMGMgMGQgMDEgMWQgNjUgNjMgNjIgMTAgNzUgMTMgMGIgMGEgMTIgMDIgMGMgNzIgMTEgMWIgMDAgNjMgMTMgMWUgMDQgMTQgNjUgMGEgMDYgNzIgMWQgMWMgMTcgNmUgMWMgMDEgNjggMTAgMGEgMGMgMTkgN2MgNjUgMTIgMGIgMDcgNzUgMDYgMGQgMTIgMTEgNjQgMDYgNzIgMTEgMWIgMDAgNjMgMTMgMWIgMGIgMTIgMDkgNjMgMTMgMWUgMDQgMTQgNjUgMGMgMTMgNzIgMTEgMWIgMGMgMTAgNzUgMTEgMGQgMTIgMDkgMGYgMTAgMWMgMDIgMTYgNjUgMGEgNzUgMWEgMGEgMDMgMDAgNjMgMGMgMWQgMTAgNzQgMTMgMDYgNzUgMTcgMGIgMTkgMGEgMWEgMTAgMTYgNjUgMTIgMGIgMDcgNzUgMWIgNjUgMTIgMDkgMTAgMWEgNzIgMGQgMWMgMTUgMDYgNzUgMDYgMGQgMWEgMTYgNjMgMWMgMDEgNjUgMTYgMGIgMGMgMDAgMTUgMGQgNzMgMTEgMDYgMGQgMDYgNjUgMDcgMGEgNjMgMTggMTMgMGUgMTYgNjUgMWIgMWEgMDAgNjUgMTEgMTcgMTYgMDEgMTcgNjUgMTUgMGEgMTEgMTYgMTcgNjUgMTIgMGIgNjMgMWEgMDIgMTEgMWEgMGEgMGQ`
 - Decodes to: `THIS XOR ENCODING STUFF IS WILD I CANT BELIVE HOW WELL IT WORKS AND HOW SECURE IT IS TOO. NOTICE HOW I USE THE KEY IN THE TEXT AND THEN THE TEXT TURNS INTO 0'S ANYWAY THE FLAG IS XOR-IS-COOL. AND THAT'S THE FINAL FLAG OF THIS CHALLENGE I HOPE YOU'VE ENJOYED AND I ALSO HOPE THIS IS ENOUGH TEXT TO MAKE XOR BRUTE FORCE AN OPTION`
-
+</details>
--- app.py
+++ app.py
@@ -53,35 +53,10 @@ not_right = [   "",
                 "Please see the following link http://nameofwebsite/help",
                 "Task failed successfully",
                 "Task successfully failed",
-                "big error, much problem",
-                """
-<html><body>
-                16.06.20 JOB00375 ---- THURSDAY,  11 MAR 2021 ----                              <br>
-16.06.20 JOB00375  IRR010I  USERID JAKE     IS ASSIGNED TO THIS JOB.            <br>
-16.06.20 JOB00375  ICH70001I JAKE     LAST ACCESS AT 16:06:19 ON THURSDAY, MARCH<br>
-16.06.20 JOB00375  $HASP373 EXECPGM  STARTED - INIT 1    - CLASS A        - SYS <br>
-16.06.20 JOB00375  CSV003I REQUESTED MODULE VOLDATA  NOT FOUND                  <br>
-16.06.20 JOB00375  CSV028I ABEND806-04  JOBNAME=EXECPGM   STEPNAME=STEP01       <br>
-16.06.20 JOB00375  IEA995I SYMPTOM DUMP OUTPUT  915                             <br>
-   915             SYSTEM COMPLETION CODE=806  REASON CODE=00000004             <br>
-   915              TIME=16.06.20  SEQ=00229  CPU=0000  ASID=001B               <br>
-   915              PSW AT TIME OF ERROR  070C1000   812D3852  ILC 2  INTC 0D   <br>
-   915                NO ACTIVE MODULE FOUND                                    <br>
-   915                NAME=UNKNOWN                                              <br>
-   915                DATA AT PSW  012D384C - 8400181E  0A0D18FB  180C181D      <br>
-   915                AR/GR 0: 001FBFFF/00001F00   1: 00000000/84806000         <br>
-   915                      2: 00000000/00000000   3: 00000000/00000000         <br>
-   915                      4: 00000000/00000000   5: 00000000/008CBEC0         <br>
-   915                      6: 00000000/000000FF   7: 00000000/00000000         <br>
-   915                      8: 00000000/7F6B5050   9: 00000000/012D3D78         <br>
-   915                      A: 00000000/00000000   B: 00000000/00000000         <br>
-   915                      C: 00000000/00000000   D: 00000000/7F6B5050         <br>
-   915                      E: 00000000/84806000   F: 00000000/00000004         <br>
-   915              END OF SYMPTOM DUMP    <br>
-</body></html>
-                """
+                "big error, much problem"
             ]
 
+errormsg = not_right + "\n" # adding newline otherwise terminal looks messy
 ############################################# custom error handling
 
 @app.route('/', methods=['GET'])
@@ -90,11 +65,11 @@ def index():
 
 @app.errorhandler(404)
 def not_found(e):
-    return pick_one(not_right)
+    return pick_one(errormsg)
 
 @app.errorhandler(405)
 def method_not_allowed(e):
-    return pick_one(not_right)
+    return pick_one(errormsg)
 
 # @app.errorhandler(500)
 # def internal_err(e):
@@ -108,7 +83,7 @@ def challenge5():
     if (os.getenv("PORT") == '8080' and re.search('(curl|wget)\/*', request.headers.get('User-Agent').lower())):
         return "Challenge String: Vjgfqgug it a qugwvy dopo vrql"
     else:
-        return pick_one(not_right)
+        return pick_one(not_right) #leaving this one as newline not required for webpage
 
 @app.route('/challenge5/hint1', methods=every_method)
 def challenge5hint1():
@@ -118,21 +93,21 @@ def challenge5hint1():
         else:
             return "Have a look into common TCP ports"
     else:
-        return "Are you using Curl or wget? I don't think you are"
+        return "I wouldn't look for hints on a cURL challenge using the browser..." # Making it more obvious that user should curl for hints
 
 @app.route('/challenge5/hint2', methods=every_method)
 def challenge5hint2():
     if (re.search('(curl|wget)\/*', request.headers.get('User-Agent').lower())):
         return "Have you heard of Vigenere?"
     else:
-        return "Are you using Curl or wget? I don't think you are"
+        return "I wouldn't look for hints on a cURL challenge using the browser..."
 
 @app.route('/challenge5/hint3', methods=every_method)
 def challenge5hint3():
     if (re.search('(curl|wget)\/*', request.headers.get('User-Agent').lower())):
         return "The code is ababdcdc"
     else:
-        return "Are you using Curl or wget? I don't think you are"
+        return "I wouldn't look for hints on a cURL challenge using the browser..."
 
 if (__name__ == '__main__' and os.getenv("PORT") == '8080'):
     app.run(debug=DEBUG, host='0.0.0.0', port=8080)
@@ -152,7 +127,7 @@ def challenge1hint1():
     if (re.search('(curl|wget)\/*', request.headers.get('User-Agent').lower())):
         return "Have a look at this https://www.seobility.net/en/wiki/GET_Parameters#Using_Get_Parameters, also think about how your Curl command is being interpreted on the CLI"
     else:
-        return "Are you using Curl or wget? I don't think you are"
+        return "I wouldn't look for hints on a cURL challenge using the browser..."
 
 @app.route('/challenge1/hint2', methods=every_method)
 def challenge1hint2():
@@ -172,11 +147,11 @@ def challenge2hint1():
     if (re.search('(curl|wget)\/*', request.headers.get('User-Agent').lower())):
         return "Make sure to run `man curl`"
     else:
-        return "Are you using Curl or wget? I don't think you are"
+        return "I wouldn't look for hints on a cURL challenge using the browser..."
 
 @app.route('/challenge2/hint2', methods=every_method)
 def challenge2hint2():
-    return "Have a look at old encoding methods, this ones only been around for almost 2000 years. Even the Romans know how to decrypt it"
+    return "Have a look at old encoding methods, this one's only been around for about 2000 years. Even the Romans know how to decrypt it"
 
 #############################################
 
@@ -192,7 +167,7 @@ def challenge3hint1():
     if (re.search('(curl|wget)\/*', request.headers.get('User-Agent').lower())):
         return "Make sure to run `man curl` to see how to set a different HTTP method"
     else:
-        return "Are you using Curl or wget? I don't think you are"
+        return "I wouldn't look for hints on a cURL challenge using the browser..."
 
 @app.route('/challenge3/hint2', methods=every_method)
 def challenge3hint2():
@@ -231,7 +206,7 @@ def challenge4hint1():
     if (re.search('(curl|wget)\/*', request.headers.get('User-Agent').lower())):
         return "Make sure to run `man curl` to see how to set content type and define a content body"
     else:
-        return "Are you using Curl or wget? I don't think you are"
+        return "I wouldn't look for hints on a cURL challenge using the browser..."
 
 @app.route('/challenge4/hint2', methods=every_method)
 def challenge4hint2():
@@ -257,10 +232,14 @@ def challenge6hint1():
     if (re.search('(curl|wget)\/*', request.headers.get('User-Agent').lower())):
         return "Make sure to run `man curl` to see how to put a file into the request body"
     else:
-        return "Are you using Curl or wget? I don't think you are"
+        return "I wouldn't look for hints on a cURL challenge using the browser..."
 
 @app.route('/challenge6/hint2', methods=every_method)
 def challenge6hint2():
+    return "There might be multiple ways of using HTTP PUT..." #Because I got confused I wanted to add this hint
+
+@app.route('/challenge6/hint3', methods=every_method)
+def challenge6hint3():
     return "Have a look for tools that help with XOR"
 
 if __name__ == '__main__':
